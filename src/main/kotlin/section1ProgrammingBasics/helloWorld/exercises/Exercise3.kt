d30f6fc6064919d6bb9d023b1297666123913886
WybsonSantana <wybson@hotmail.com>
2023-04-21 15:12:16 -0300

    Section I - Programming Basics > Hello Exercise 3

---
+++ src/main/kotlin/section1ProgrammingBasics/helloWorld/exercises/Exercise3.kt
@@ -0,0 +1,15 @@
+package section1ProgrammingBasics.helloWorld.exercises
+
+/*
+Modify the previous exercise by commenting the line that displays "Hello".
+Verify that line is not executed.
+*/
+
+fun main() {
+    //println("Hello,")
+    println("Kotlin!")
+}
+
+/*
+Kotlin!
+*/
