4135bc7cab974b1ac6900e4abecc296f66e051fc
WybsonSantana <wybson@hotmail.com>
2023-04-21 14:57:37 -0300

    Section I - Programming Basics > Hello Exercise 1

---
+++ src/main/kotlin/section1ProgrammingBasics/helloWorld/exercises/Exercise1.kt
@@ -0,0 +1,14 @@
+package section1ProgrammingBasics.helloWorld.exercises
+
+/*
+Create and run a program that displays the phrase "Hello, Kotlin!" on the
+console.
+*/
+
+fun main() {
+    println("Hello, Kotlin!")
+}
+
+/* Output:
+Hello, Kotlin!
+*/
