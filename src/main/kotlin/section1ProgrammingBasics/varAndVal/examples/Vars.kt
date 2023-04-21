1947b6769cc65f34cfe59d04e839b635a0d18092
WybsonSantana <wybson@hotmail.com>
2023-04-21 15:31:38 -0300

    Section I - Programming Basics > Var & Val > Vars

---
+++ src/main/kotlin/section1ProgrammingBasics/varAndVal/examples/Vars.kt
@@ -0,0 +1,16 @@
+package section1ProgrammingBasics.varAndVal.examples
+
+fun main() {
+    var whole = 11
+    var fractional = 1.4
+    var words = "Twas Brillig"
+    println(whole)
+    println(fractional)
+    println(words)
+}
+
+/* Output:
+11
+1.4
+Twas Brillig
+*/
