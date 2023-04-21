c7bd4016b0cbc2bf965d056fe76c8b78bc2e758c
WybsonSantana <wybson@hotmail.com>
2023-04-21 19:04:00 -0300

    Section I - Programming Basics > Functions > Exercise 1

---
+++ src/main/kotlin/section1ProgrammingBasics/functions/exercises/Exercise1.kt
@@ -0,0 +1,16 @@
+package section1ProgrammingBasics.functions.exercises
+
+/*
+Create a function `squareInt()` that takes an `Int` parameter and
+returns its mathematical square.
+*/
+
+fun squareInt(x: Int) = x * x
+
+fun main() {
+    println(squareInt(9))
+}
+
+/* Output:
+81
+*/
