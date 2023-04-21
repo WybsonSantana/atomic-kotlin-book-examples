ae904d843dc0170c1f70c6e5887aa16fdd9c2238
WybsonSantana <wybson@hotmail.com>
2023-04-21 16:32:37 -0300

    Section I - Programming Basics > Var & Val > Exercise 5

---
+++ src/main/kotlin/section1ProgrammingBasics/varAndVal/exercises/Exercise5.kt
@@ -0,0 +1,23 @@
+package section1ProgrammingBasics.varAndVal.exercises
+
+/*
+The icons at the top of the Project Tool Window
+perform several convenient operations. For example, you can locate the
+currently opened file in the Project Tool Window
+by clicking on the Select Opened File icon.
+
+Press the Collapse All button on the
+Project Tool Window to hide all the
+project content, then open the current file in the list.
+
+You can find more details in the documentation
+*/
+
+fun main() {
+    val message = "Hello, Kotlin!"
+    println(message)
+}
+
+/* Output:
+Hello, Kotlin!
+*/
