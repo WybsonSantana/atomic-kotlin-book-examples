df9a4c2013754d79ffdf792455341feb08dfbfbd
WybsonSantana <wybson@hotmail.com>
2023-04-21 15:42:32 -0300

    Section I - Programming Basics > Var & Val > Vals

---
+++ src/main/kotlin/section1ProgrammingBasics/varAndVal/examples/Vals.kt
@@ -0,0 +1,17 @@
+package section1ProgrammingBasics.varAndVal.examples
+
+fun main() {
+    val whole = 11
+    //whole = 15  // Error: Val cannot be reassigned
+    val fractional = 1.4
+    val words = "Twas Brillig"
+    println(whole)
+    println(fractional)
+    println(words)
+}
+
+/* Output:
+11
+1.4
+Twas Brillig
+*/
