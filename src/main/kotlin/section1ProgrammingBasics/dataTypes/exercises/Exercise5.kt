35a627329daaec5c63db2956592ab51a4917322c
WybsonSantana <wybson@hotmail.com>
2023-04-21 18:26:40 -0300

    Section I - Programming Basics > Data Types > Exercise 5

---
+++ src/main/kotlin/section1ProgrammingBasics/dataTypes/exercises/Exercise5.kt
@@ -0,0 +1,18 @@
+package section1ProgrammingBasics.dataTypes.exercises
+
+/*
+IntelliJ IDEA helps you automatically add type specifications for vals and vars.
+Put the caret right after s and press &shortcut:ShowIntentionActions;.
+This is the shortcut for Show intention actions.
+Choose Specify type explicitly in the dropdown menu.
+Different intention actions are available in different contexts, depending on where you put the caret
+*/
+
+fun main() {
+    val string: String = "Sally" + 5.9
+    println(string)
+}
+
+/* Output:
+Sally5.9
+*/
