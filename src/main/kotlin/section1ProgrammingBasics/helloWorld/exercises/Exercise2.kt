63ff3e2fa5e5868bd6445c67e96dbe6ac476d7ae
WybsonSantana <wybson@hotmail.com>
2023-04-21 15:03:15 -0300

    Section I - Programming Basics > Hello Exercise 2

---
+++ src/main/kotlin/section1ProgrammingBasics/helloWorld/exercises/Exercise2.kt
@@ -0,0 +1,21 @@
+package section1ProgrammingBasics.helloWorld.exercises
+
+/*
+Display "Hello, Kotlin!" on the console, but with "Kotlin" appearing on the
+next line. You can display two separate `String`s to achieve this:
+
+```text
+Hello,
+Kotlin!
+```
+*/
+
+fun main() {
+    println("Hello,")
+    println("Kotlin!")
+}
+
+/* Output:
+Hello,
+Kotlin!
+*/
