2bfd610edb7ce82c429383be4403eeed7520371e
WybsonSantana <wybson@hotmail.com>
2023-04-21 16:24:24 -0300

    Section I - Programming Basics > Var & Val > Exercise 4

---
+++ src/main/kotlin/section1ProgrammingBasics/varAndVal/exercises/Exercise4.kt
@@ -0,0 +1,22 @@
+package section1ProgrammingBasics.varAndVal.exercises
+
+/*
+The Project window showing the course structure
+on the left and the Task window on the right are
+called tool windows. You can adjust their size by clicking on the
+boundary and moving it. You can temporarily hide the tool window from the view
+to make better use of the screen: click the "hide" button in the top right
+corner of the tool window. To see it again, choose its name by clicking on the
+right or left side of the screen or by choosing it in the
+View | Tool Windows application menu.
+Hide the Task tool window and then open it again.
+*/
+
+fun main() {
+    val message = "Hello, Kotlin!"
+    println(message)
+}
+
+/* Output:
+Hello, Kotlin!
+*/
