2c495694b4162ad49e67469a4af0836cbdbb55fb
WybsonSantana <wybson@hotmail.com>
2023-04-23 19:13:10 -0300

    Section I - Programming Basics > If Expressions > One or the Other

---
+++ src/main/kotlin/section1ProgrammingBasics/ifExpressions/examples/OneOrTheOther.kt
@@ -0,0 +1,18 @@
+package section1ProgrammingBasics.ifExpressions.examples
+
+fun oneOrTheOther(exp: Boolean): String =
+    if (exp)
+        "True!"
+    else
+        "False!"
+
+fun main() {
+    val x = 1
+    println(oneOrTheOther(x == 1))
+    println(oneOrTheOther(x == 2))
+}
+
+/* Output:
+True!
+False!
+*/
