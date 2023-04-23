30f17e29eb1e7ddef87828968c6290550a77ab86
WybsonSantana <wybson@hotmail.com>
2023-04-23 19:00:59 -0300

    Section I - Programming Basics > If Expressions > If 6

---
+++ src/main/kotlin/section1ProgrammingBasics/ifExpressions/examples/If6.kt
@@ -0,0 +1,13 @@
+package section1ProgrammingBasics.ifExpressions.examples
+
+fun main() {
+    val num = 10
+
+    val result = if (num > 100) 4 else 42
+
+    println(result)
+}
+
+/* Output:
+42
+*/
