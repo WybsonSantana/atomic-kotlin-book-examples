b575b20b1d177e07b67e4d906db6c875dafa77c6
WybsonSantana <wybson@hotmail.com>
2023-04-23 19:50:38 -0300

    Section I - Programming Basics > String Templates > String Concatenation

---
+++ src/main/kotlin/section1ProgrammingBasics/stringTemplates/examples/StringConcatenation.kt
@@ -0,0 +1,14 @@
+package section1ProgrammingBasics.stringTemplates.examples
+
+fun main() {
+    val string = "Hi\n"
+    val int = 11
+    val double = 3.14
+
+    println("first: " + string + "second: " + int + ", third: " + double)
+}
+
+/* Output:
+first: Hi
+second: 11, third: 3.14
+*/
