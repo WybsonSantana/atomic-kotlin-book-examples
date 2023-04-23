fd4b0609857a29a780766eae27510fb053069c60
WybsonSantana <wybson@hotmail.com>
2023-04-23 18:51:44 -0300

    src/main/kotlin/section1ProgrammingBasics/ifExpressions/examples/If3.kt

---
+++ src/main/kotlin/section1ProgrammingBasics/ifExpressions/examples/If3.kt
@@ -0,0 +1,14 @@
+package section1ProgrammingBasics.ifExpressions.examples
+
+fun main() {
+    val n: Int = -11
+
+    if (n > 0)
+        println("It's positive!")
+    else
+        println("It's negative or zero!")
+}
+
+/* Output:
+It's negative or zero!
+*/
