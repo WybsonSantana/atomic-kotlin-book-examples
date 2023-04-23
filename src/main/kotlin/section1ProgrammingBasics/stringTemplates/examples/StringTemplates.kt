1c375e8c4d13ed19e3fa40c63b91d1ddf7d1d448
WybsonSantana <wybson@hotmail.com>
2023-04-23 19:42:42 -0300

    Section I - Programming Basics > String Templates > String Templates

---
+++ src/main/kotlin/section1ProgrammingBasics/stringTemplates/examples/StringTemplates.kt
@@ -0,0 +1,12 @@
+package section1ProgrammingBasics.stringTemplates.examples
+
+fun main() {
+    val answer = 42
+    println("Found $answer!")
+    println("Printing a $1...")
+}
+
+/* Output:
+Found 42!
+Printing a $1...
+*/
