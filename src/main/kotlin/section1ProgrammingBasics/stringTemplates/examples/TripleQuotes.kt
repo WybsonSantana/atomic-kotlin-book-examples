261274f312a1b056d64868ee48a62599818cbf92
WybsonSantana <wybson@hotmail.com>
2023-04-23 22:05:04 -0300

    Section I - Programming Basics > String Templates > Triple Quotes

---
+++ src/main/kotlin/section1ProgrammingBasics/stringTemplates/examples/TripleQuotes.kt
@@ -0,0 +1,13 @@
+package section1ProgrammingBasics.stringTemplates.examples
+
+fun main() {
+    val string = "Value"
+
+    println("String = \"$string\".")
+    println("""String = "$string".""")
+}
+
+/* Output:
+String = "Value".
+String = "Value".
+*/
