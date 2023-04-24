3e39b437f808f4079e4c2089d88e712f08f9459a
WybsonSantana <wybson@hotmail.com>
2023-04-23 22:46:18 -0300

    Section I - Programming Basics > String Templates > Exercise 4

---
+++ src/main/kotlin/section1ProgrammingBasics/stringTemplates/exercises/Exercise4.kt
@@ -0,0 +1,27 @@
+package section1ProgrammingBasics.stringTemplates.exercises
+
+/*
+You can search for and replace any text in a file.
+Press &shortcut:Find; for Find and
+&shortcut:Replace; for Replace.
+
+Replace all instances of foo in the file with bar.
+
+Note that you can press &shortcut:Find;,
+start typing, then press &shortcut:Replace;
+and the history is preserved.
+Pressing &shortcut:Replace; adds a field
+where you can type the replace string.
+
+Click on the small arrow to the left of the typing window to see the search history.
+*/
+
+fun foo() = 1
+
+fun main() {
+    println(foo())
+}
+
+/* Output:
+1
+*/
